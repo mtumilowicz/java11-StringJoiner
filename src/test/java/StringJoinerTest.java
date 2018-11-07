b5c13e018e9207e64cf205f1eb88e40cb17f6c70
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-07 13:53:07 +0100

    tests

--- src/test/java/StringJoinerTest.java
+++ src/test/java/StringJoinerTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 
 import java.util.List;
 import java.util.StringJoiner;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -9,13 +10,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Created by mtumilowicz on 2018-11-07.
  */
 public class StringJoinerTest {
-    
+
     @Test
     public void commaSeparated() {
         var stringJoiner = new StringJoiner(",");
-        
+
         List.of("a", "b", "c").forEach(stringJoiner::add);
-        
+
         assertThat(stringJoiner.toString(), is("a,b,c"));
     }
 
@@ -27,24 +28,24 @@ public class StringJoinerTest {
 
         assertThat(stringJoiner.toString(), is("prefix-a,b,c-suffix"));
     }
-    
+
     @Test
     public void merge() {
         var stringJoinerComma = new StringJoiner(",");
         List.of("a", "b", "c").forEach(stringJoinerComma::add);
-        
+
         var stringJoinerDot = new StringJoiner(".");
         List.of("d", "e", "f").forEach(stringJoinerDot::add);
 
         var merged = stringJoinerComma.merge(stringJoinerDot);
-        
+
         assertThat(merged.toString(), is("a,b,c,d.e.f"));
     }
-    
+
     @Test
     public void empty() {
         var stringJoiner = new StringJoiner(",");
-        
+
         assertThat(stringJoiner.toString(), is(""));
     }
 
@@ -54,14 +55,14 @@ public class StringJoinerTest {
 
         assertThat(stringJoiner.toString(), is("prefix-suffix"));
     }
-    
+
     @Test
     public void setEmptyValues() {
         var stringJoiner = new StringJoiner(",");
         stringJoiner.setEmptyValue("empty");
-        
+
         assertThat(stringJoiner.toString(), is("empty"));
-        
+
     }
 
     @Test
@@ -70,7 +71,6 @@ public class StringJoinerTest {
         stringJoiner.setEmptyValue("empty");
 
         assertThat(stringJoiner.toString(), is("empty"));
-
     }
-    
+
 }
